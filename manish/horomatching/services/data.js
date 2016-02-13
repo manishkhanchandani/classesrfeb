78938b8d46911039555cc158b339158a2c8648f5
Manish Khanchandani <manishkk74@gmail.com>
2016-02-13 13:54:29 -0800

    data service changed

--- manish/horomatching/services/data.js
+++ manish/horomatching/services/data.js
@@ -8,15 +8,22 @@ angular.module('myApp').service('dataService', ['$http', function($http) {
       }).then(callback, callbackFailed);
   };
   
-  this.post = function(url, data, callback, callbackFailed, cache) {
+  //data: 'email='+encodeURIComponent($scope.frm.email)+'&question='+encodeURIComponent($scope.frm.question)+'&name='+encodeURIComponent($scope.frm.name)
+  this.post = function(url, data, callback, callbackFailed) {
       $http({
         method: 'POST',
         url: url,
         data: data,
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        cache: cache
-        //headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      //data: 'email='+encodeURIComponent($scope.frm.email)+'&question='+encodeURIComponent($scope.frm.question)+'&name='+encodeURIComponent($scope.frm.name)
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(callback, callbackFailed);
+  };
+  
+  //data: {key: value, key: value}
+  this.postJson = function(url, data, callback, callbackFailed) {
+      $http({
+        method: 'POST',
+        url: url,
+        data: data
       }).then(callback, callbackFailed);
   };
 }]);
