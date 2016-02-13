95c9c7e287fad7e6e38cdd6d873adad6dc432a87
Raghu <ragmurug@cisco.com>
2016-02-13 13:38:25 -0800

    Local Storage of User for Horo

--- raghu/horomatching/app.js
+++ raghu/horomatching/app.js
@@ -3,12 +3,13 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'ngAutocomplete',
-  'ui.bootstrap'
+  'myApp.view1'
+ // ,
+//  'ngAutocomplete',
+//  'ui.bootstrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }])
 
 .controller('mainController', ['$scope', function($scope) {
--- raghu/horomatching/view1/view1.js
+++ raghu/horomatching/view1/view1.js
@@ -1,14 +1,56 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ngAutocomplete'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+  $routeProvider.when('/', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
 
+  $scope.profile = {
+    'name': 'Manish',
+    'day': 5,
+    'month': 6,
+    'year': 1974,
+    'hour': 12,
+    'minute': 30
+    };
+  
+  $scope.mapOptions = {
+      types: '(cities)'
+    };
+
+  $scope.details = {};
+  
+  function saveUser(profile) {
+    //get the profile
+    var profiles = localStorage.getItem('profiles');
+    var key = btoa(profile.name);
+    var obj = {};
+    if (!profiles) {
+      obj[key] = profile;
+      localStorage.setItem('profiles', JSON.stringify(obj));
+    } else {
+      obj = JSON.parse(profiles);
+      if (obj[key]) {
+        console.log('user already existed');
+      } else {
+        obj[key] = profile;
+        localStorage.setItem('profiles', JSON.stringify(obj));
+      }
+    }
+    return obj;
+  }//end saveUser
+
+  //form submission
+  $scope.profileSubmit = function() {
+    $scope.profile.lat = $scope.details.components.lat;
+    $scope.profile.lng = $scope.details.components.lng;
+    //saving user in local storage
+    saveUser($scope.profile);
+  };
 }]);
