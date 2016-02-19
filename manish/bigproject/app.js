c338a749189ca373d2a0a405d8cc967310ec092a
Manish Khanchandani <manishkk74@gmail.com>
2016-02-18 21:06:21 -0800

    more

---
+++ manish/bigproject/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1'
+  //,
+  //'ngAutocomplete',
+  //'ui.bootstrap'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+
+.controller('mainController', ['$scope', function($scope) {
+
+}])
