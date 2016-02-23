d1acff0a71e150e03cae1116039343adba95880b
Christine Zhang <czhang@pernixdata.com>
2016-02-23 10:56:04 -0800

    auth

--- chris/bigproject/app.js
+++ chris/bigproject/app.js
@@ -3,17 +3,20 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.view3',
-  'myApp.view4',
-  'myApp.view5'
+    "myApp.lessons",
+    "myApp.auth"
+//  'myApp.view1',
+//  'myApp.view2',
+//  'myApp.view3',
+//  'myApp.view4',
+//  'myApp.view5',
+//  'myApp.view6'
   //,
   //'ngAutocomplete',
   //'ui.bootstrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/lessons'});
 }])
 
 .controller('mainController', ['$scope', function($scope) {
