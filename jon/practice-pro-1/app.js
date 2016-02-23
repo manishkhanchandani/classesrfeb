f3c55f03d787ff7ff27184e59794daf90a8ea13d
Jonathan415 <upthere415415@hotmail.com>
2016-02-22 20:50:55 -0800

    update

--- jon/practice-pro-1/app.js
+++ jon/practice-pro-1/app.js
@@ -3,11 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.view3',
-  'myApp.view4',
-  'myApp.view5',
+  'myApp.auth',
+  'myApp.lessons'
 
   //,
   //'ngAutocomplete',
