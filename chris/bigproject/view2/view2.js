20492376ece5335da196c4ebb68fb71f892640f0
Christine Zhang <czhang@pernixdata.com>
2016-02-22 20:08:00 -0800

    5 views

---
+++ chris/bigproject/view2/view2.js
@@ -0,0 +1,14 @@
+'use strict';
+// this is a sub module
+angular.module('myApp.view2', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', ['$scope', function($scope) {
+
+}]);
