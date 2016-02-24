6c99ef8f225198afc4fb734fd15d38f2255fdd16
aruna <harsha.kallur@gmail.com>
2016-02-23 17:03:26 -0800

    committing big project auth

--- aruna/bigproject/modules/auth/auth.js
+++ aruna/bigproject/modules/auth/auth.js
@@ -11,16 +11,32 @@ angular.module('myApp.auth', ['ngRoute'])
 
 .controller('ViewAuthCtrl', ['$scope',function($scope) {
  var ref = new Firebase("https://glowing-inferno-3312.firebaseio.com");
+ $scope.createUserError = null;
       $scope.createNewUser = function() {
+          
+          if ($scope.frm.confirm_password !== $scope.frm.password) {
+       $scope.createUserError = 'Password does not match with confirm password. Please check again!';
+       return;
+    }
+
+
    ref.createUser({
       email    : $scope.frm.email,
       password : $scope.frm.password
     }, function(error, userData) {
       if (error) {
         console.log("Error creating user:", error);
+        $scope.createUserError = "Error creating user:" + error;
+
       } else {
         console.log("Successfully created user account with uid:", userData.uid);
+        $scope.createUserError = "Successfully created user account with uid:" + userData.uid;
+        $scope.frm={}; 
       }
+      //to update the scope in html page
+if(!$scope.$$phase) $scope.$apply();
+
+
     });
   };
 
