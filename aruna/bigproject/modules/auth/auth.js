3ed3659d8bb789c92b21bee53133ba627519448f
aruna <harsha.kallur@gmail.com>
2016-02-26 10:52:32 -0800

    committing big project

--- aruna/bigproject/modules/auth/auth.js
+++ aruna/bigproject/modules/auth/auth.js
@@ -20,26 +20,31 @@ angular.module('myApp.auth', ['ngRoute'])
   
   $scope.frmLogin = {};
   
-  $scope.loginUser = function() {
-    $scope.loginUser = function() {
+  
+   $scope.loginUser = function() {
     ref.authWithPassword({
       email    : $scope.frmLogin.email,
       password : $scope.frmLogin.password
     }, function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
+        $scope.loginError = "Login Failed!" + error;
       } else {
         console.log("Authenticated successfully with payload:", authData);
+        $scope.loginError = "Authenticated successfully with uid:" + authData.uid;
+        $scope.frmLogin = {};
       }
+      if(!$scope.$$phase) $scope.$apply();
     });
   };
 
-  };
+
+ 
   
 }])
 
 
-.controller('ViewAuthCreateCtrl', ['$scope',function($scope) {
+.controller('ViewAuthCreateCtrl', ['$scope','dataService',function($scope,dataService) {
  var ref = new Firebase("https://glowing-inferno-3312.firebaseio.com");
  $scope.createUserError = null;
       $scope.createNewUser = function() {
@@ -48,7 +53,27 @@ angular.module('myApp.auth', ['ngRoute'])
        $scope.createUserError = 'Password does not match with confirm password. Please check again!';
        return;
     }
+    console.log($scope.frm);
+    
+    function createUserSuccess(response) {
+      console.log('success results: ', response);
+    }
+    
+    function createUserFailure(response) {
+      console.log('failure results: ', response);
+    }
 
+    
+  var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/login.php?action=register&saveIP=1';
+    var postData = 'email='+encodeURIComponent($scope.frm.email)+'&password='+encodeURIComponent($scope.frm.password)+
+            '&uid=100&username='+encodeURIComponent($scope.frm.username)+'&user_details[fullname]='
+            +encodeURIComponent($scope.frm.fullname)+'&user_details[age]='+encodeURIComponent($scope.frm.age);
+    
+     dataService.post(url, postData, createUserSuccess, createUserFailure);
+
+
+
+return;
 
    ref.createUser({
       email    : $scope.frm.email,
