f810181e4be612f7620b55021340b4296f8142de
Christine Zhang <czhang@pernixdata.com>
2016-02-26 20:47:07 -0800

    backend login

--- chris/bigproject/modules/auth/auth.js
+++ chris/bigproject/modules/auth/auth.js
@@ -13,14 +13,30 @@ angular.module('myApp.auth', ['ngRoute'])
   });
 }])
 
-.controller('ViewAuthLoginCtrl', ['$scope', function($scope) {
-  var ref = new Firebase("https://cguo.firebaseio.com");
+.controller('ViewAuthLoginCtrl', ['$scope', 'dataService', function($scope, dataService) {
+  //var ref = new Firebase("https://cguo.firebaseio.com");
   $scope.loginError = null;
   
   $scope.frmLogin = {};
+      function loginUserSuccess(response){
+          console.log('success results: ', response);
+          if (response.data.error === 1) {
+              $scope.loginError = response.data.errorMessage;
+              return;
+          }
+          $scope.loginError = 'New user created successfully.';
+          $scope.frmLogin = {};
+      }
+      function loginUserFailure(response){
+          console.log('failure results: ', response);
+          $scope.loginError = 'Failed to create user.';
+      }
   
   $scope.loginUser = function() {
-    ref.authWithPassword({
+      var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/login.php?action=login&saveIP=1';
+      var postData = 'userName='+encodeURIComponent($scope.frmLogin.email)+'&password='+encodeURIComponent($scope.frmLogin.password);
+      dataService.post(url, postData, loginUserSuccess, loginUserFailure);
+    /*ref.authWithPassword({
       email    : $scope.frmLogin.email,
       password : $scope.frmLogin.password
     }, function(error, authData) {
@@ -34,19 +50,40 @@ angular.module('myApp.auth', ['ngRoute'])
         //login service from our DB, ajax in Anguler
       }
       if(!$scope.$$phase) $scope.$apply();
-    });
+    });*/
   };
   
 }])
 
-.controller('ViewAuthCtrl', ['$scope', function($scope) {
-  var ref = new Firebase("https://cguo.firebaseio.com");
+.controller('ViewAuthCtrl', ['$scope', 'dataService', function($scope, dataService) {
+//  var ref = new Firebase("https://cguo.firebaseio.com");
   $scope.createUserError = null;
+      function createUserSuccess(response){
+          console.log('success results: ', response);
+          if (response.data.error === 1) {
+              $scope.createUserError = response.data.errorMessage;
+              return;
+          }
+          $scope.createUserError = 'New user created successfully.';
+          $scope.frm = {};
+      }
+      function createUserFailure(response){
+          console.log('failure results: ', response);
+          $scope.createUserError = 'Failed to create user.';
+      }
   $scope.createNewUser = function() {
       if ($scope.frm.confirm_password !== $scope.frm.password) {
        $scope.createUserError = 'Password does not match with confirm password. Please check again!';
        return;
         }
+      
+      console.log($scope.frm);
+      // change the url to make a failure case.
+      var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/login.php?action=register&saveIP=1';
+      var postData = 'email='+encodeURIComponent($scope.frm.email)+'&password='+encodeURIComponent($scope.frm.password)+'&username='+encodeURIComponent($scope.frm.username)+'&user_details[fullname]='+encodeURIComponent($scope.frm.fullname)+'&user_details[age]='+encodeURIComponent($scope.frm.age);
+      dataService.post(url, postData, createUserSuccess, createUserFailure);
+/*      return;
+      
       ref.createUser({
           email    : $scope.frm.email,
           password : $scope.frm.password
@@ -63,6 +100,6 @@ angular.module('myApp.auth', ['ngRoute'])
           //to update the scope in html page, when using 3rd party tool, or regular JS in AngularJS, e.g. firebase here
           if(!$scope.$$phase) $scope.$apply();
 
-        });
+        });*/
   };
 }]);
