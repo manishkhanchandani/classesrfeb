1221675c71efefa6f6abdfc5f39198be09c9474a
Manish Khanchandani <manishkk74@gmail.com>
2016-02-28 12:39:11 -0800

    more

---
+++ manish/examples/htmledit/view1/view1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+$scope.htmlContent = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE9+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
+}]);
--- manish/examples/sampleProject/modules/lessons/lessons.js
+++ manish/examples/sampleProject/modules/lessons/lessons.js
@@ -33,8 +33,8 @@ angular.module('myApp.lessons', ['ngRoute'])
 .controller('ViewLessonsCtrl', ['$scope', function($scope) {
   
 }])
-.controller('ViewCreateLessonsCtrl', ['$scope', function($scope) {
-    $scope.frm = {};
+.controller('ViewCreateLessonsCtrl', ['$scope', 'dataService', '$location', function($scope, dataService, $location) {
+    $scope.frmAdd = {};
     
     //location starts
     $scope.mapOptions = {
@@ -43,13 +43,53 @@ angular.module('myApp.lessons', ['ngRoute'])
 
     $scope.details = {};
     //location ends
+    
+    $scope.createError = null;
+    
+    function addSuccess(response) {
+      console.log('success: ', response);
+      $location.path('/lessons/create/images/'+response.data.data.id);
+      
+    }
+    
+    function addFailure(response) {
+      console.log('failure: ', response);
+    }
   
     $scope.mainDetails = function() {
-       console.log($scope.frm);
+      console.log($scope.frmAdd);
+      console.log($scope.loggedInUsersData);
+      var submitData = '';
+      submitData = submitData + '&title='+encodeURIComponent($scope.frmAdd.title);
+      //xtra data
+      submitData = submitData + '&description='+encodeURIComponent($scope.frmAdd.description);
+      //location
+      submitData = submitData + '&location[latitude]='+encodeURIComponent($scope.details.components.lat);
+      submitData = submitData + '&location[longitude]='+encodeURIComponent($scope.details.components.lng);
+      submitData = submitData + '&location[country]='+encodeURIComponent($scope.details.components.country);
+      submitData = submitData + '&location[state]='+encodeURIComponent($scope.details.components.state);
+      submitData = submitData + '&location[city]='+encodeURIComponent($scope.details.components.city);
+      submitData = submitData + '&location[zip]='+($scope.details.components.zip ? encodeURIComponent($scope.details.components.zip) : '');
+      submitData = submitData + '&location[place_id]='+encodeURIComponent($scope.details.place_id);
+      submitData = submitData + '&location[county]='+encodeURIComponent($scope.details.components.county);
+      submitData = submitData + '&location[formatted_addr]='+encodeURIComponent($scope.details.formatted_address);
+      
+      //tags
+      submitData = submitData + '&tags='+($scope.frmAdd.tags ? encodeURIComponent($scope.frmAdd.tags) : '');
+      //url
+      var access_token = $scope.loggedInUsersData.token;
+      var path = '/manish/lessons';
+      var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/records.php?action=add&saveIP=1&access_token='+access_token+'&path='+path;
+      dataService.post(url, submitData, addSuccess, addFailure);
     };
 }])
-.controller('ViewImagesLessonsCtrl', ['$scope', function($scope) {
+.controller('ViewImagesLessonsCtrl', ['$scope', 'dataService', '$location', '$routeParams', function($scope, dataService, $location, $routeParams) {
+  console.log($routeParams);
+  $scope.frmAdd = {
+    id: $routeParams.id  
+  };
   
+  console.log($scope.frmAdd);
 }])
 .controller('ViewImagesUrlsLessonsCtrl', ['$scope', function($scope) {
   
