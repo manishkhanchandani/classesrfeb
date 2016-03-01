995f42bc66cae9ae7aed9996ffb0d9a8e3832e2a
Jonathan415 <upthere415415@hotmail.com>
2016-02-29 20:41:36 -0800

    update

--- jon/practice-pro-1/app.js
+++ jon/practice-pro-1/app.js
@@ -11,9 +11,11 @@ angular.module('myApp', [
   //'ui.bootstrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }])
 
 .controller('mainController', ['$scope', function($scope) {
-  
+    //create global variable that can be access anywhere
+    $scope.loggedInUsersData    = null;
+
 }])
