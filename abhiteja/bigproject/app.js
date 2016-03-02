35181103c51bb35c75d6cbf90b699616bf989f85
abhiteja <abhitejag@gmail.com>
2016-03-02 11:27:54 -0800

    user logout changes

--- abhiteja/bigproject/app.js
+++ abhiteja/bigproject/app.js
@@ -17,4 +17,11 @@ angular.module('myApp',
 
 .controller('mainController', ['$scope', function($scope) {
     $scope.loggedInUserData = null;
+    //getting details from localStorage
+    var userProfile = localStorage.getItem('userProfile');
+    if(userProfile){
+        $scope.loggedInUserData = JSON.parse(userProfile);
+    }
+    console.log(userProfile);
+    
 }]);
