e633d78a9002622db183196eccd0f78b58a99442
Jonathan415 <upthere415415@hotmail.com>
2016-03-01 20:46:17 -0800

    update

--- jon/practice-pro-1/app.js
+++ jon/practice-pro-1/app.js
@@ -1,4 +1,5 @@
 'use strict';
+// app.js is global and will be called on every page refresh.
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
@@ -17,5 +18,15 @@ config(['$routeProvider', function($routeProvider) {
 .controller('mainController', ['$scope', function($scope) {
     //create global variable that can be access anywhere
     $scope.loggedInUsersData    = null;
+
+    //Get the details from local storage
+    var userProfile                 = localStorage.getItem('userProfile');
+
+    console.log(userProfile);
+
+    if( userProfile ){
+        $scope.loggedInUsersData    = JSON.parse(userProfile);
+    }
+
 
 }])
