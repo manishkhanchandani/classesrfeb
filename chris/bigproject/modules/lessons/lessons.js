584fd2096aa3fea81b6576f0854a32a742232267
Christine Zhang <czhang@pernixdata.com>
2016-03-02 20:55:20 -0800

    location in lessons/create

--- chris/bigproject/modules/lessons/lessons.js
+++ chris/bigproject/modules/lessons/lessons.js
@@ -6,9 +6,24 @@ angular.module('myApp.lessons', ['ngRoute'])
   $routeProvider.when('/lessons', {
     templateUrl: 'modules/lessons/lessons.html',
     controller: 'ViewLessonsCtrl'
+  }).when('/lessons/create', {
+    templateUrl: 'modules/lessons/create.html',
+    controller: 'ViewCreateCtrl'
+  }).when('/lessons/create/images:id', {  //need id from page1
+    templateUrl: 'modules/lessons/images.html',
+    controller: 'ViewImagesCtrl'
   });
 }])
 
 .controller('ViewLessonsCtrl', ['$scope', function($scope) {
+
+}]).controller('ViewCreateCtrl', ['$scope', function($scope) {
+    //location starts 
+    $scope.mapOptions = { 
+        types: 'geocode' 
+    }; 
+    $scope.details = {}; 
+    //location ends
+}]).controller('ViewImagesCtrl', ['$scope', function($scope) {
 
 }]);
