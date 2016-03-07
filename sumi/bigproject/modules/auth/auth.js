9b487516a2a1515a97e48c0f536f1ec57b0809cb
SumiParuchuri <sumiparuchuri1991@gmail.com>
2016-03-06 21:26:10 -0600

    problem with create user

--- sumi/bigproject/modules/auth/auth.js
+++ sumi/bigproject/modules/auth/auth.js
@@ -14,73 +14,61 @@ angular.module('myApp.auth', ['ngRoute'])
   });
 }])
 
-.controller('ViewAuthLoginCtrl', ['$scope', function($scope) {
+.controller('ViewAuthLoginCtrl', ['$scope', 'dataService',function($scope,dataService) {
   $scope.loginError = null;
+ 
+    function loginSuccess(response) {
+    console.log('success: ', response);
+  }
+  
+  function loginFailure(response) {
+    console.log('failure: ', response);
+  }
+  
   $scope.loginUser = function() {
-    /*ref.authWithPassword({
-    email    : $scope.frmLogin.email,
-    password : $scope.frmLogin.password
-      }, 
-        function(error, authData) {
-            if (error) {
-              console.log("Login Failed!", error);
-              $scope.loginError = "Login Failed!"+ error;
-            } else {
-              console.log("Authenticated successfully with payload:", authData);
-              $scope.loginError = "Authenticated successfully with payload:"+ authData.uid;
-               $scope.frmLogin={};
-            }            
-    if(!$scope.$$phase) $scope.$apply();
-    $scope.frmLogin = {};
-    });*/
+    var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/login.php?action=login&saveIP=1';
+    var postData = 'username='+encodeURIComponent($scope.frmLogin.email)+'&password='+encodeURIComponent($scope.frmLogin.password);
+    dataService.post(url, postData, loginSuccess, loginFailure);
   };
-  }])
+  
+}])
 
-.controller('ViewAuthCreateCtrl',['$scope','dataService',function($scope,dataService) {
-       
-        $scope.createUserError= null;   
-        
-        function createUserSuccess(response) {
-            console.log('success results: ', response);
-            if (response.data.error === 1) {
-              $scope.createUserError = response.data.errorMessage;
-             
-            }
-            $scope.createUserError= "New User Created Successfully";            
-        }  
-        function createUserFailure(response) {
-          console.log('failure results: ', response);
-          $scope.createUserError = 'Failed to create user. Please try again';
-        }
+.controller('ViewAuthCreateCtrl', ['$scope', 'dataService', function($scope, dataService) {
 
-        $scope.createNewUser = function(){
-        
-        if($scope.frm.confirm_password !== $scope.frm.password){
-            
-            $scope.createUserError="Confirm password doesn't match with password. Please check once!";
-            return;            
-        }        
-        var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/login.php?action=register&saveIP=1';
-        var postData = 'email='+encodeURIComponent($scope.frm.email)+'&password='+encodeURIComponent($scope.frm.password)+'&user_details[fullname]='+encodeURIComponent($scope.frm.fullname)+'&user_details[age]='+encodeURIComponent($scope.frm.age);
-        dataService.post(url, postData, createUserSuccess, createUserFailure);
-        };
+
+  $scope.createUserError = null;
+
+
+  function createUserSuccess(response) {
+    console.log('success results: ', response);
+    if (response.data.error === 1) {
+      $scope.createUserError = response.data.errorMessage;
+      return;
+    }
     
-     }]);
-       /*
-        *  var ref = new Firebase("https://vivid-torch-274.firebaseio.com");
-        * ref.createUser({
-      email    : $scope.frm.email,
-      password : $scope.frm.password
-        }, function(error, userData) {
-          if (error) {
-            console.log("Error creating user:", error);
-            $scope.createUserError="Error creating user:"+ error;
-          } else {
-            console.log("Successfully created user account with uid:", userData.uid);
-            $scope.createUserError="Successfully created user account with uid:"+ userData.uid;
-            $scope.frm={};
-          }
-          if(!$scope.$$phase) $scope.$apply();
-        });
+    $scope.createUserError = 'New User Created Successfully.';
+    $scope.frm = {};
+  }
+  
+  function createUserFailure(response) {
+    console.log('failure results: ', response);
+    $scope.createUserError = 'Failed to create user. Please try again';
+  }
+  
+  $scope.createNewUser = function() {
+    if ($scope.frm.confirm_password !== $scope.frm.password) {
+       $scope.createUserError = 'Password does not match with confirm password. Please check again!';
+       return;
+    }
     
-    };*/
+    console.log($scope.frm);
+    
+    
+    var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/login.php?action=register&saveIP=1';
+    var postData = 'email='+encodeURIComponent($scope.frm.email)+'&password='+encodeURIComponent($scope.frm.password)+'&username='+encodeURIComponent($scope.frm.username)+'&user_details[fullname]='+encodeURIComponent($scope.frm.fullname)+'&user_details[age]='+encodeURIComponent($scope.frm.age);
+    
+    dataService.post(url, postData, createUserSuccess, createUserFailure);
+  };
+  
+  
+}])
