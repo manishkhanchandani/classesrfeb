c03ef600bf881a2a78aca46c862cbdf5505c9212
Manish Khanchandani <manishkk74@gmail.com>
2016-03-07 20:44:49 -0800

    more

--- manish/bigproject/modules/lessons/lessons.js
+++ manish/bigproject/modules/lessons/lessons.js
@@ -32,7 +32,25 @@ angular.module('myApp.lessons', ['ngRoute'])
   $scope.submitCreateForm = function() {
      //call api service to submit the form
      
-     $location.path('/lessons/create/images/1');
+     //$location.path('/lessons/create/images/1');
+     console.log($scope.loggedInUsersData);
+     console.log($scope.frm);
+     console.log($scope.details);
+     
+     
+     var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/records.php?action=add&saveIP=1&access_token='+$scope.loggedInUsersData.token;
+     
+     console.log(url);
+     
+     /*&title=This+is+title&path=/test&tid=1&location[latitude]=37.7974273&location[longitude]=-121.21605260000001&location[country]=United+States&location[state]=CA&location[city]=Manteca&location[zip]=&location[place_id]=ChIJCUWMJENAkIARjMxOe6Wp4p0&location[county]=San+Joaquin+County&location[formatted_addr]=Manteca,+CA,+United+States&tags=a,b,c&description=some+description
+
+&data[gender]=Male
+*/
+    var postData = '';
+    postData = postData + '&title='+encodeURIComponent($scope.frm.title);
+    
+    console.log(postData);
+     
   };
 }])
 
