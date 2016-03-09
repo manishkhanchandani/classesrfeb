02885ec2804f6644bec83893c0d6ddbbe0c867f8
aruna <harsha.kallur@gmail.com>
2016-03-08 21:25:47 -0800

    committing craigslist 5 projects

--- aruna/bigproject/modules/activities/activities.js
+++ aruna/bigproject/modules/activities/activities.js
@@ -22,7 +22,7 @@ angular.module('myApp.activities', ['ngRoute'])
 
 }])
 
-.controller('ViewActivitiesCreateCtrl', ['$scope','$location',function($scope,$location) {
+.controller('ViewActivitiesCreateCtrl', ['$scope','$location','dataService',function($scope,$location,dataService) {
 //location starts
   $scope.mapOptions = {
     types: 'geocode'
@@ -31,10 +31,46 @@ angular.module('myApp.activities', ['ngRoute'])
   $scope.details = {};
   //location ends
   
+  function addSuccess(response) {
+    console.log('success: ', response);
+    console.log('id is : ', response.data.data.id);
+    $scope.frm = {};
+    $location.path('/activities/create/images/'+response.data.data.id);
+  }
+  
+  function addFailure(response) {
+    console.log('failure: ', response);
+  }
+  
    $scope.submitActivitiesCreateForm = function() {
      //call api service to submit the form
-     console.log('hey');
-     $location.path('/activities/create/images/1');
+      var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/records.php?action=add&saveIP=1&access_token='+$scope.loggedInUsersData.token+'&path=/aruna/activities&tid=1';
+     
+     console.log(url);
+     
+    
+    var postData = '';
+    postData = postData + '&title='+encodeURIComponent($scope.frm.title);
+    postData = postData + '&description='+encodeURIComponent($scope.frm.description);
+    
+    postData = postData + '&location[latitude]='+encodeURIComponent($scope.details.components.lat);
+    postData = postData + '&location[longitude]='+encodeURIComponent($scope.details.components.lng);
+    postData = postData + '&location[country]='+encodeURIComponent($scope.details.components.country);
+    postData = postData + '&location[state]='+encodeURIComponent($scope.details.components.state);
+    postData = postData + '&location[city]='+encodeURIComponent($scope.details.components.city);
+    postData = postData + '&location[zip]='+encodeURIComponent($scope.details.components.postal_code);
+    postData = postData + '&location[place_id]='+encodeURIComponent($scope.details.place_id);
+    postData = postData + '&location[county]='+encodeURIComponent($scope.details.components.county);
+    postData = postData + '&location[formatted_addr]='+encodeURIComponent($scope.details.formatted_address);
+    
+    postData = postData + '&tags='+encodeURIComponent($scope.frm.tags);
+    
+    postData = postData + '&data[gender]='+encodeURIComponent($scope.frm.gender);
+    
+    console.log(postData);
+    dataService.post(url, postData, addSuccess, addFailure);
+ 
+   
   };
 }])
 
--- aruna/bigproject/modules/legal/legal.js
+++ aruna/bigproject/modules/legal/legal.js
@@ -20,7 +20,7 @@ angular.module('myApp.legal', ['ngRoute'])
 .controller('ViewLegalCtrl', ['$scope',function($scope) {
 
 }])
-.controller('ViewLegalCreateCtrl', ['$scope','$location',function($scope,$location) {
+.controller('ViewLegalCreateCtrl', ['$scope','$location','dataService',function($scope,$location,dataService) {
  //location starts
   $scope.mapOptions = {
     types: 'geocode'
@@ -29,10 +29,47 @@ angular.module('myApp.legal', ['ngRoute'])
   $scope.details = {};
   //location ends
   
+  function addSuccess(response) {
+    console.log('success: ', response);
+    console.log('id is : ', response.data.data.id);
+    $scope.frm = {};
+    $location.path('/legal/create/images/'+response.data.data.id);
+  }
+  
+  function addFailure(response) {
+    console.log('failure: ', response);
+  }
+  
    $scope.submitLegalCreateForm = function() {
      //call api service to submit the form
-     console.log('hello');
-     $location.path('/legal/create/images/1');
+      var url = 'http://bootstrap.mkgalaxy.com/svnprojects/horo/records.php?action=add&saveIP=1&access_token='+$scope.loggedInUsersData.token+'&path=/aruna/legal&tid=1';
+     
+     console.log(url);
+     
+    
+    var postData = '';
+    postData = postData + '&title='+encodeURIComponent($scope.frm.title);
+    postData = postData + '&description='+encodeURIComponent($scope.frm.description);
+    
+    postData = postData + '&location[latitude]='+encodeURIComponent($scope.details.components.lat);
+    postData = postData + '&location[longitude]='+encodeURIComponent($scope.details.components.lng);
+    postData = postData + '&location[country]='+encodeURIComponent($scope.details.components.country);
+    postData = postData + '&location[state]='+encodeURIComponent($scope.details.components.state);
+    postData = postData + '&location[city]='+encodeURIComponent($scope.details.components.city);
+    postData = postData + '&location[zip]='+encodeURIComponent($scope.details.components.postal_code);
+    postData = postData + '&location[place_id]='+encodeURIComponent($scope.details.place_id);
+    postData = postData + '&location[county]='+encodeURIComponent($scope.details.components.county);
+    postData = postData + '&location[formatted_addr]='+encodeURIComponent($scope.details.formatted_address);
+    
+    postData = postData + '&tags='+encodeURIComponent($scope.frm.tags);
+    
+    postData = postData + '&data[gender]='+encodeURIComponent($scope.frm.gender);
+    
+    console.log(postData);
+    dataService.post(url, postData, addSuccess, addFailure);
+ 
+    
+     
   };
 
 }])
