20cd94ecceb357c4ca7a53f2b22d430ecbc1d7f1
Manish Khanchandani <manishkk74@gmail.com>
2016-03-26 23:28:35 -0700

    more

--- manish/homeopathyrx.tk/app.js
+++ manish/homeopathyrx.tk/app.js
@@ -11,6 +11,20 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
 }])
 
+.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+})
+
 .controller('mainController', ['$scope', function($scope) {
   
 }])
--- manish/homeopathyrx.tk/view1/view1.js
+++ manish/homeopathyrx.tk/view1/view1.js
@@ -28,16 +28,26 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 
-.controller('ViewRxCtrl', ['$scope', 'dataService', '$routeParams', function($scope, dataService, $routeParams) {
+.controller('ViewRxCtrl', ['$scope', 'dataService', '$routeParams', '$location', function($scope, dataService, $routeParams, $location) {
   $scope.frm = {};
   
   $scope.results = {};
   $scope.hom = {};
+  $scope.remedyResults = null;
+  $scope.queryResults = null;
   
   function getSuccess(response) {
-    console.log('success: ', response);
+    //console.log('success: ', response);
     $scope.results = response.data.data.diseases;
     $scope.hom = response.data.data.hom;
+    if ($routeParams.disease_id) {
+      $scope.queryResults = $scope.hom[$routeParams.disease_id];
+      $scope.frm.disease = $scope.results[$routeParams.disease_id];
+      if ($routeParams.rid) {
+        $scope.remedyResults = $scope.hom[$routeParams.disease_id][$routeParams.rid];
+        $scope.frm.tongue = $scope.hom[$routeParams.disease_id][$routeParams.rid];
+      }
+    }
   }
   
   function getFailure(response) {
@@ -48,25 +58,101 @@ angular.module('myApp.view1', ['ngRoute'])
   dataService.get(url, getSuccess, getFailure, true);
   
   
-  $scope.queryResults = null;
   
   $scope.updateDisease = function() {
-    $scope.queryResults = null;
-    $scope.remedyResults = null;
+    //$scope.queryResults = null;
+    //$scope.remedyResults = null;
     if (!$scope.frm.disease) return;
-    console.log($scope.hom[$scope.frm.disease.disease_id]);
-    $scope.queryResults = $scope.hom[$scope.frm.disease.disease_id];
+    $location.path('/rx/'+$scope.frm.disease.disease_id);
+    //$scope.queryResults = $scope.hom[$scope.frm.disease.disease_id];
   };
   
   
-  $scope.remedyResults = null;
   
   $scope.updateRemedy = function() {
-    $scope.remedyResults = null;
     if (!$scope.frm.disease) return;
-    if (!$scope.frm.tongue && !$scope.frm.pulse) return;
-    $scope.remedyResults = $scope.hom[$scope.frm.disease.disease_id][$scope.frm.tongue.id];
+    if (!$scope.frm.tongue) return;
+    $location.path('/rx/'+$scope.frm.disease.disease_id+'/'+$scope.frm.tongue.id);
+    //$scope.remedyResults = $scope.hom[$scope.frm.disease.disease_id][$scope.frm.tongue.id];
   };
+  
+  $scope.profiles = null;
+  $scope.showProfiles = function() {
+    var profiles = localStorage.getItem('profiles');
+    $scope.profiles = JSON.parse(profiles);
+  };
+  
+  $scope.saveFrm = {};
+  
+  var dt = new Date();
+  var day = dt.getDate();
+  var month = dt.getMonth() + 1;
+  var year = dt.getFullYear();
+  $scope.saveFrm.date = year + '-' + month + '-' + day;
+    
+  $scope.saveRemedy = function() {
+    if (!$scope.saveFrm.name) {
+     return; 
+    }
+    if (!$scope.saveFrm.date) {
+     return; 
+    }
+    
+    var dt = new Date();
+    var day = dt.getDate();
+    var month = dt.getMonth() + 1;
+    var year = dt.getFullYear();
+    var hour = dt.getHours();
+    var minute = dt.getMinutes();
+    var seconds = dt.getSeconds();
+    var createdDt = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + seconds;
+    var key = btoa($scope.saveFrm.name + createdDt);
+    var profile = {
+      createdDt: $scope.saveFrm.date,
+      formattedDt: dt.toString(),
+      name: $scope.saveFrm.name,
+      path: '/rx/'+$scope.frm.disease.disease_id+'/'+ $scope.frm.tongue.id,
+      disease_id: $scope.frm.disease.disease_id,
+      id: $scope.frm.tongue.id,
+      remedy: $scope.remedyResults.remedies,
+      key: key,
+      disease: $scope.frm.disease.disease,
+      pulse: $scope.frm.tongue.pulse,
+      tongue: $scope.frm.tongue.tongue,
+      tcd: $scope.frm.tongue.tcd
+    };
+    
+    var obj = {};
+    var profiles = localStorage.getItem('profiles');
+    if (!profiles) {
+      obj[key] = profile;
+      localStorage.setItem('profiles', JSON.stringify(obj));
+    } else {
+      obj = JSON.parse(profiles);
+      if (!obj[key]) {
+        obj[key] = profile;
+        localStorage.setItem('profiles', JSON.stringify(obj));
+      }  
+    }
+    
+    $scope.saveFrm.name = '';
+    $scope.showProfiles();
+  };
+  
+  $scope.showProfiles();
+  
+  $scope.deleteProfile = function(p) {
+    var a = confirm('do you really want to delete this profile?');
+    if (!a) return false;
+    var obj = {};
+    var profiles = localStorage.getItem('profiles');
+    obj = JSON.parse(profiles);
+    delete obj[p.key];
+    localStorage.setItem('profiles', JSON.stringify(obj));
+    $scope.showProfiles();
+  };
+  
+  
 }])
 
 
