200446be6dfe6ae9bf7cf570eb49167aae4ff847
Raghu <ragmurug@cisco.com>
2016-03-31 22:42:18 -0700

    BigProject - Location Form in Lessons Module

--- raghu/bigproject/modules/lessons/lessons.js
+++ raghu/bigproject/modules/lessons/lessons.js
@@ -3,12 +3,35 @@
 angular.module('myApp.lessons', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/lessons', {
+  $routeProvider
+	
+	.when('/lessons', {
     templateUrl: 'modules/lessons/lessons.html',
     controller: 'ViewLessonsCtrl'
-  });
+  })
+	.when('/lessons/create', {
+    templateUrl: 'modules/lessons/create.html',
+    controller: 'ViewCreateCtrl'
+  })
+	.when('/lessons/images/:id', {
+    templateUrl: 'modules/lessons/images.html',
+    controller: 'ViewImagesCtrl'
+  })
+	;
 }])
 
 .controller('ViewLessonsCtrl', ['$scope', function($scope) {
-  
-}]);
+	
+}])
+.controller('ViewCreateCtrl', ['$scope', function($scope) {
+	$scope.mapOptions = {
+		types: 'geocode'
+	};
+	
+	$scope.details = {};
+		
+}])
+.controller('ViewImagesCtrl', ['$scope', function($scope) {
+	
+}])
+;
