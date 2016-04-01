7a31235c59b0ed3d06811433f2e6cce648492b7f
Manish Khanchandani <manishkk74@gmail.com>
2016-03-31 20:39:44 -0700

    form for firebase

--- manish/fb1/view1/view1.js
+++ manish/fb1/view1/view1.js
@@ -11,4 +11,29 @@ angular.module('myApp.view1', ['ngRoute', 'firebase'])
 
 .controller('View1Ctrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
   var ref = new Firebase('https://mycontacts12.firebaseio.com/fb1');
+  
+  $scope.records = {};
+  $scope.frm = {};
+  $scope.frmEdit = {};
+  
+  //add new records
+  $scope.addForm = function() {
+    
+  };
+  
+  //when user fills the edit form and submit the data, it will update the server
+  $scope.editForm = function() {
+    
+  };
+  
+  //go to server, get the record and show record in edit form
+  $scope.editLink = function() {
+    
+  };
+  
+  //go to server, delete the record
+  $scope.deleteLink = function() {
+    
+  };
+  
 }]);
