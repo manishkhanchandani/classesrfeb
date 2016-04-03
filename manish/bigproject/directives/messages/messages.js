03945930b1a12908bea94c74fa4583e0aaa9e1aa
Manish Khanchandani <manishkk74@gmail.com>
2016-04-03 15:38:14 -0700

    messags

--- manish/bigproject/directives/messages/messages.js
+++ manish/bigproject/directives/messages/messages.js
@@ -10,7 +10,7 @@
   }
   
   module
-    .directive('messages', ['messagesTemplate', 'messagesService', 'configs', '$location', 'dataService', '$firebaseArray', messages])
+    .directive('messages', ['messagesTemplate', 'messagesService', 'configs', '$location', 'dataService', '$firebaseArray', '$timeout', messages])
     .provider('messagesTemplate', messagesTemplateProvider)
     .service('messagesService', ['$http', messagesService])
     
@@ -77,7 +77,7 @@
     };
   }
   
-  function messages(messagesTemplate, messagesService, configs, $location, dataService, $firebaseArray) {
+  function messages(messagesTemplate, messagesService, configs, $location, dataService, $firebaseArray, $timeout) {
     return {
           scope: {
             userData: '=',
@@ -92,7 +92,7 @@
                  window.history.back();
                  return;
               }
-              console.log('from user: ', scope.userData);
+              //console.log('from user: ', scope.userData);
   
               if (scope.routeParams.uid) {
                 if (scope.userData.id === scope.routeParams.uid) {
@@ -101,18 +101,33 @@
                 }
               }
               
+              scope.badges = {};
+              
               //firebase reference
               var host = $location.host();
               var ref = new Firebase(configs[host].firebaseUrl);
+              var badge = ref.child('badges');
   
+              //reset badge
+              badge.child(scope.userData.id).child(scope.routeParams.uid).child('badge').set(0);
+              
               //from user
               var usersFrom = ref.child('users').child(scope.userData.id);
               var queryFrom = usersFrom.orderByChild("timestamp").limitToLast(500);
               scope.usersFrom = $firebaseArray(queryFrom);
   
+              /*usersFrom.on('value', function(snapshot) {
+                angular.forEach(snapshot.val(), function(value, key) {
+                  badge.child(scope.userData.id).child(key).on("value", function(snapshot2) {
+                    scope.badges[snapshot2.key()] = snapshot2.val();
+                  });
+                });
+              });*/
+              
               //to user
               if (scope.routeParams.uid) {
                 var usersTo = ref.child('users').child(scope.routeParams.uid);
+                
                 var queryTo = usersTo.orderByChild("timestamp").limitToLast(500);
                 scope.usersTo = $firebaseArray(queryTo);
                 //adding from - to user
@@ -122,7 +137,7 @@
               function getSuccess(response) {
                 var results = response.data.data;
                 scope.resultsToUser = results;
-                console.log('to user: ', scope.resultsToUser);
+                //console.log('to user: ', scope.resultsToUser);
                 //adding from - to user
                 usersFrom.child(scope.routeParams.uid).set({email: results.user_details.email, id: results.uid, name: results.user_details.name, image: results.user_details.image, url: results.user_details.url, timestamp: Firebase.ServerValue.TIMESTAMP});
               }
@@ -142,10 +157,22 @@
               
               //messages
               var mes = ref.child('messages');
-              var badge = ref.child('badges');
               var queryMes = mes.child(scope.userData.id).child(scope.routeParams.uid).orderByChild("timestamp").limitToLast(100);
               scope.messages = $firebaseArray(queryMes);
               
+              //badge
+              badge.child(scope.userData.id).on("value", function(snapshot) {
+                angular.forEach(snapshot.val(), function (value, key) {
+                  scope.badges[key] = value;
+                });
+                //if(!scope.$$phase) scope.$apply();
+                $timeout(function(){
+                    //any code in here will automatically have an apply run afterwards
+                    if(!scope.$$phase) scope.$apply();
+                }, 3000);
+              });
+              //badge ends
+                            
               scope.sendMessage = function(fid, tid, name, image) {
                 //type: 2 = sent, 1 = received
                 var newPostRef = mes.child(tid).child(fid).push({message: scope.frm.message, timestamp: Firebase.ServerValue.TIMESTAMP, type: 1, name: name, image: image, read: false});
@@ -154,7 +181,9 @@
                 
                 scope.frm.message = '';
                 //badge
-                badge.child(tid).child(fid).transaction(function(currentValue) {
+                badge.child(fid).child(tid).child('badge').set(0);
+                
+                badge.child(tid).child(fid).child('badge').transaction(function(currentValue) {
                     return (currentValue||0) + 1
                 }, function(err, committed, ss) {
                     if( err ) {
@@ -163,7 +192,7 @@
                     else if( committed ) {
                        // if counter update succeeds, then create record
                        // probably want a recourse for failures too
-                       badge.child(tid).child(fid).set(ss.val()); 
+                       badge.child(tid).child(fid).child('badge').set(ss.val()); 
                     }
                 });
               };//badge ends
