d60e817e488c95b7d75e84494f2dfcb271163faa
deepti <deepti.skarora@gmail.com>
2016-04-12 20:18:13 -0700

    excericise 2 view1a

--- deepti/p1/view1/view1.js
+++ deepti/p1/view1/view1.js
@@ -6,9 +6,16 @@ angular.module('myApp.view1', ['ngRoute'])
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
+  })
+    .when('/view1a', {
+    templateUrl: 'view1/view1a.html',
+    controller: 'View1aCtrl'
   });
 }])
 
 .controller('View1Ctrl', [function() {
+
+}])
+.controller('View1aCtrl', [function() {
 
 }]);
