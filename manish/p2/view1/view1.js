aa008c4decbf639a42f462aa9c53656fd809dbd0
Manish Khanchandani <manishkk74@gmail.com>
2016-05-09 21:53:08 -0700

    angular2seed

---
+++ manish/p2/view1/view1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  })
+  .when('/login', {
+    templateUrl: 'view1/login.html',
+    controller: 'ViewLoginCtrl'
+  })
+  ;
+}])
+
+.controller('View1Ctrl', [function() {
+
+}])
+
+.controller('ViewLoginCtrl', ['$scope', function($scope) {
+
+}])
+;
