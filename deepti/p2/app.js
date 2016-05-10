12b4599d327fc5904d1b536726b99fbad8ed44d4
deepti <deepti.skarora@gmail.com>
2016-05-09 22:20:04 -0700

    login.html

---
+++ deepti/p2/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'ngAutocomplete'
+  //'ui.bootstrap'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+
+.controller('mainController', ['$scope', function($scope) {
+    var ref = new Firebase("https://amber-torch-2130.firebaseio.com");
+    ref.createUser({
+      email    : "a4@mkgalaxy.com",
+      password : "password"
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+  
+}])
