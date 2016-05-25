1e63e837f65b50fd8e4e71ac311366e64ed6c0aa
Manish Khanchandani <manishkk74@gmail.com>
2016-05-25 08:54:31 -0700

    more

--- manish/lawcourse/directives/raw/raw.js
+++ manish/lawcourse/directives/raw/raw.js
@@ -1,1 +1,29 @@
-// JavaScript Document
+(function() {
+
+  var moduleName = 'rawModule';
+  var module;
+  try {
+      module = angular.module(moduleName);
+  } catch(err) {
+      // named module does not exist, so create one
+      module = angular.module(moduleName, []);
+  }
+  
+  module
+    .directive('rawAdd', ['dataService', 'configs', rawAdd])
+    ;
+    
+  
+  function rawAdd(dataService, configs) {
+    return {
+          scope: {
+            userData: '='
+          },
+          templateUrl: 'directives/raw/add.html',
+          link: function(scope, elem, attrs) {
+              
+          }//end link
+      };//end return
+  }
+
+}());
