51661012f32b37eb28ed856acd47bc15bd521897
Manish Khanchandani <manishkk74@gmail.com>
2016-12-07 20:55:07 -0800

    mylocalcity

---
+++ manish/mylocalcity/public/README.md
@@ -0,0 +1,61 @@
+https://github.com/firebase/angularfire
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# yatai-healthprint
+
+git clone https://manish-shaklee@github.com/shaklee/yatai-healthprint.git
+
+it will ask your password, once you provide the password, it will install all files.
+
+edit /usr/local/zend/apache2/conf/extra/httpd-vhosts.conf
+
+
+
+<VirtualHost *:80>
+    ServerAdmin mkhancha-contractor@shaklee.com
+    DocumentRoot "/Users/mkhancha/web/shakleegit/yatai-healthprint"
+    ServerName yatai-healthprint
+	SetEnv HOSTNAME yatai-healthprint
+    <Directory "/Users/mkhancha/web/shakleegit/yatai-healthprint">
+        Options FollowSymLinks
+        AllowOverride All
+        Order allow,deny
+        Allow from all
+    </Directory>
+</VirtualHost>
+
+
+/etc/hosts
+127.0.0.1 yatai-healthprint
+
+Go to URL
+http://yatai-healthprint/
+
---
+++ manish/mylocalcity/public/app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'pascalprecht.translate',
+  'myApp.view1',
+  'firebase'
+  //'ui.bootstrap'
+])
+
+.factory('MyErrorHandler', function ($q, $log) {
+  return function (part, lang, response) {
+    $log.error('The "' + part + '/' + lang + '" part was not loaded.');
+    return $q.when({});
+  };
+})
+
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.otherwise({redirectTo: '/'});
+  //$locationProvider.html5Mode(true);
+}])
+
+.config(['$translateProvider', '$translatePartialLoaderProvider', function($translateProvider, $translatePartialLoaderProvider) {
+  //different language files can be defined here as part
+  $translatePartialLoaderProvider.addPart('a');
+  $translatePartialLoaderProvider.addPart('b');
+  //useloader to hand the template for translation
+  $translateProvider.useLoader('$translatePartialLoader', {
+    urlTemplate: '/i18n/{lang}/{part}.json',
+    loadFailureHandler: 'MyErrorHandler'
+  });
+  var prefLang = localStorage.getItem('preferredLanguage');
+  if (!prefLang) prefLang = 'us-en';
+  $translateProvider.preferredLanguage(prefLang);
+}])
+
+.controller('mainController', ['$scope', '$translate', '$routeParams', function($scope, $translate, $routeParams) {
+  //firebase init
+  // Initialize the Firebase SDK
+  var config = {
+    apiKey: 'AIzaSyDnERUhALUFNxWZsjaLpT4_nqIYW2i2jDU',
+    authDomain: 'mkgxy-3d7ce.firebaseapp.com',
+    databaseURL: 'https://mkgxy-3d7ce.firebaseio.com/',
+    storageBucket: 'mkgxy-3d7ce.appspot.com'
+  };
+  firebase.initializeApp(config);
+  //end firebase
+  
+  //language change
+  $scope.changeLanguage = function (langKey) {
+    $translate.use(langKey);
+    localStorage.setItem('preferredLanguage', langKey);
+    
+    var tmp = langKey.split('-');
+    $scope.country = tmp[0];
+    $scope.language = tmp[1];
+  };//end function changeLanguage
+  
+  $scope.country = 'us';
+  $scope.language = 'en';
+  
+  //check language based on routeParams and change if needed
+  $scope.checkLanguage = function(params) {
+    if (!(params.country && params.language)) {
+      return;
+    }//end if
+    
+    $scope.country = params.country;
+    $scope.language = params.language;
+    
+    var langKey = params.country + '-' + params.language;
+    if (!(langKey === 'us-en' || langKey === 'us-es' || langKey === 'ca-en' || langKey === 'ca-fr')) {
+      langKey = 'us-en';
+    }
+    $scope.changeLanguage(langKey);
+  };//end function checkLanguage
+  
+  
+}])
