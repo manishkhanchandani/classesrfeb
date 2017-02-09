c2187c010240415d934402e604909835d60a8583
Manish Khanchandani <manishkk74@gmail.com>
2017-02-08 22:42:47 -0800

    more

---
+++ manish/angular212prjs/project1/app/courses.component.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var CoursesComponent;
+    return {
+        setters:[],
+        execute: function() {
+            CoursesComponent = (function () {
+                function CoursesComponent() {
+                }
+                return CoursesComponent;
+            }());
+            exports_1("CoursesComponent", CoursesComponent);
+        }
+    }
+});
+//# sourceMappingURL=courses.component.js.map
---
+++ manish/angular212prjs/project1/app/courses.component.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+
+
+export class CoursesComponent {
+
+}
