163f640d816c66a2aea4f55aab73be40ae711a4e
Manish Khanchandani <manishk@chegg.com>
2017-07-31 19:07:18 -0700

    tort 1

---
+++ manish/course/law/revision/tort1.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Torts Concepts Part 1</title>
+</head>
+
+<body>
+<div class="containter">
+<h1>Torts</h1>
+<h3>Checklist</h3>
+<ul>
+  <li><strong>Intentional Torts And Defenses</strong>
+<ul>
+      <li>Memorize By Element / IRAC ABC FITT
+        <ul>
+          <li>Assault</li>
+          <li>Battery</li>
+          <li>Conversion</li>
+          <li>False Imprisonment</li>
+          <li>Intentional Inflection of Emotional Distress</li>
+          <li>Trespass to Land</li>
+          <li>Trespass to Chattel</li>
+        </ul>
+      </li>
+      <li><strong>Defenses:</strong> Crazy Silly Double Dealing Dirty Dog Leave Right Now Never Return Ever
+        <ul>
+          <li>Consent</li>
+          <li>Self Defense</li>
+          <li>Defense of Others</li>
+          <li>Defense of Property</li>
+          <li>Discipline</li>
+          <li>Detention For Investigation</li>
+          <li>Legal Authority</li>
+          <li>Reentry of Land Wrongfully Withheld</li>
+          <li>Necessity (Public)</li>
+          <li>Necessity (Private)</li>
+          <li>Recapture of Chattel Wrongfully Withheld</li>
+          <li>Entry to Abate A Nuisance</li>
+        </ul>
+      </li>
+      </ul>
+  </li>
+  <li><strong>Negligence/ Defenses</strong>
+<ul>
+      <li>Prima Facia Case
+        <ul>
+          <li>Duty</li>
+          <li>Breach</li>
+          <li>Causation - Actual / Proximate</li>
+          <li>Damages</li>
+        </ul>
+      </li>
+      <li>Defenses
+        <ul>
+          <li>Contributory</li>
+          <li>Comparative</li>
+          <li>Last Clear Chance</li>
+          <li>Assumption of Risk</li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+  <li><strong>Miscellaneous Tort Concepts</strong>
+<ul>
+      <li>Wrongful Death</li>
+      <li>Survival Statutes</li>
+      <li>Statute of Limitations</li>
+      <li>Immunities</li>
+    </ul>
+  </li>
+  <li><strong>Strict Liability</strong>
+<ul>
+      <li>Animals</li>
+      <li>Ultrahazardous / Abnormally Dangerous Activties</li>
+    </ul>
+  </li>
+  <li><strong>Vicarious Liability</strong>
+<ul>
+      <li>Employment - Course / Scope</li>
+      <li>Borrowed Servant Doctrine</li>
+      <li>Independent Contractors</li>
+      <li>Joint Venture</li>
+      <li>Family Purpose Doctrine / Permissive Use Statutes</li>
+      <li>Bailor / Bailee</li>
+      <li>Parent Child</li>
+    </ul>
+  </li>
+  <li><strong>Products Liability</strong>
+<ul>
+      <li>Intentional (rare)</li>
+      <li>Negligence</li>
+      <li>Warranty</li>
+      <li>Strict Liability in Tort</li>
+    </ul>
+  </li>
+  <li><strong>Crossover Torts</strong>
+<ul>
+      <li>Nuisance
+        <ul>
+          <li>Beware of Remedies</li>
+        </ul>
+      </li>
+      <li>Misrepresentation
+        <ul>
+          <li>Beware of Contracts / Remedies</li>
+        </ul>
+      </li>
+      <li>Business Torts
+        <ul>
+          <li>Beware of Contracts / Remedies</li>
+        </ul>
+      </li>
+      <li>Imporper Litigation</li>
+    </ul>
+  </li>
+  <li><strong>Defamation</strong>
+<ul>
+      <li>Prima Facie Case</li>
+      <li>Damages - Libel / Slander</li>
+      <li>Defenses</li>
+    </ul>
+  </li>
+  <li><strong>Invasion of Privacy</strong>
+<ul>
+      <li>Appropriation of Likeness</li>
+      <li>Intrusion Upon Seclusion</li>
+      <li>Public Disclosure of Private Facts</li>
+      <li>False Light in the Public Eye</li>
+      <li>Defenses</li>
+    </ul>
+  </li>
+  <li><strong>Tort Remedies</strong>
+  <ul>
+    <li>Damages
+      <ul>
+        <li>General</li>
+        <li>Special</li>
+        <li>Punitive</li>
+        <li>Nominal</li>
+        </ul>
+      </li>
+    <li>Restitution</li>
+    <li>Injuction</li>
+    </ul>
+  </li>
+</ul>
+<p><strong>Transferred Intent</strong><br>
+D's wrongful intent is transferred from the <strong>intended victim</strong> to <strong>actual victim</strong> or from the <strong>intended tort</strong> to the <strong>committed tort</strong>. </p>
+<p><strong>Note:</strong> Modernly limited to Assault, Battery and False Imprisonment</p>
+<p><strong>INTENTIONAL TORTS</strong></p>
+<p><strong>Assault: </strong>The intentional placing of another in reasonable apprehension of an imminent harmful or offensive touching without consent or privilege.</p>
+<ol>
+  <li>Intentional</li>
+  <li>Placing of another in reasonable apprehension</li>
+  <li>Of an imminent harmful or offensive touching</li>
+  <li>Without concent or privilege</li>
+</ol>
+<p><strong>Battery: </strong>The intentional harmful or offensive touching of another without consent or privilege.</p>
+<ol>
+  <li>Intentional</li>
+  <li>Harmful or Offensive Touching of Another</li>
+  <li>Without Consent or Privilege</li>
+</ol>
+<p><strong>False Imprisonment: </strong>The intentional physical or psychological confinement of another within fixed boundaries, for any period of time, without consent or privilege.</p>
+<ol>
+  <li>Intentional</li>
+  <li>Physical or Psychological Confinement of Another</li>
+  <li>Within Fixed Boundaries</li>
+  <li>For Any Period of Time</li>
+  <li>Without Consent or Privilege</li>
+</ol>
+<p><strong>Intentional Infliction of Emotional Distress:</strong> Conduct of an extreme and outrageous nature which is calculated to cause and which does cause severe emotional distress.</p>
+<ol>
+  <li>Conduct of an Exreme and Outrageous nature</li>
+  <li>Which is Calculated to Cause</li>
+  <li>And Which Does Cause</li>
+  <li>Severe Emotional Distress</li>
+</ol>
+<p><strong>Trespass to land: </strong>The intentional entry upon the land in possession of another without consent or privilege.</p>
+<ol>
+  <li>Intentional</li>
+  <li>Entry Upon Land</li>
+  <li>In Possession of Another</li>
+  <li>Without Consent or Privilege</li>
+</ol>
+<p><strong>Trespass to Chattel:</strong> The intentional interference with the chattel in possession of another without consent or privilege.</p>
+<ol>
+  <li>Intentional</li>
+  <li>Interference With Chattel</li>
+  <li>In Possession of Another</li>
+  <li>Without Consent Or Privilege</li>
+</ol>
+<p><strong>Conversion: </strong>The intentional exercise of wrongful dominion and control over the chattel of another without consent or privilege.</p>
+<ol>
+  <li>Intentional</li>
+  <li>Exercise of Wrongful Dominion And Control</li>
+  <li>Over the Chattel of Another</li>
+  <li>Without Consent Or Privilege</li>
+</ol>
+<p>DEFENSES</p>
+<p><strong>Consent: </strong>Actual, apparent and implied by law (save a life)</p>
+<p><strong>Selft Defense:</strong> Reasonable force used to repel attack.</p>
+<p><strong>Defense of Others:</strong> Reasonable force to protect any third person from harm.</p>
+<ul>
+  <li>Majority: Step into the shoes of victim.</li>
+  <li>Minority: Reasonable mistake protected.</li>
+  </ul>
+<p><strong>Defense of Property:</strong> Non - deadly force is ok</p>
+<p><strong>Discipline:</strong> Reasonable forece by one in loco parentis with the plaintiff</p>
+<p><strong>Detention for Investigation:</strong> Shopkeepr Privilege</p>
+<ul>
+  <li>Reasonable grounds</li>
+  <li>Reasonable amount of time</li>
+  <li>Reasonable force</li>
+  <li>Reasonable investigation</li>
+  <li>Majority will permit a reasonable mistake</li>
+</ul>
+<p><strong>Legal Authority:</strong> Reasonable force, and deadly force only if person poses imminent threat to society or the officer.</p>
+<p><strong>Reentry of land wrongfully withheld:</strong></p>
+<ul>
+  <li>Majority: No privilege exists to use any force</li>
+  <li>Minority: Allows reasonable force to regain possession.</li>
+  </ul>
+<p><strong>Necessity (Public)</strong></p>
+<p>Complete defense and use of deadly force is allowed</p>
+<p><strong>Necessity (Private)</strong></p>
+<ul>
+  <li>Incomplete defense (D is liable for damages)</li>
+  <li>Entry must be reasonable</li>
+  </ul>
+<p><strong>Recapture of Chattel wrongfully withheld</strong></p>
+<p>Reasonable force in fresh pursuit, Entry made in reasonable manner, reasonable force, is complete defense.</p>
+<p><strong>Entry to abate a nuisance (nuisances can be intentional)</strong></p>
+<p>Defendant, must be owner, first make a demand to stop nuisance, and use reasonable time and reasonable force to effect the abatement.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+</body>
+</html>
