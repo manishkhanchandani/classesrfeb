23561178f4a27d580e89fa8448499487df787d04
Manish Khanchandani <manishk@chegg.com>
2017-08-10 09:21:10 -0700

    more

---
+++ manish/course/law/revision/tortEssay.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>Tort Essay</p>
+<p>A. Intentional Torts</p>
+<p>B. Negligence</p>
+<p>C. Products Liability</p>
+<p>D. Strict Liability</p>
+<p>E. Defamation</p>
+<p>F. Vicarious Liability</p>
+<p>G. Invasion of Privacy</p>
+<p><strong>Tort Essay Samples</strong></p>
+<p><strong>A. Intentional Torts</strong></p>
+<p><strong>Approach</strong></p>
+<ul>
+  <li>Intent</li>
+  <li>ABC-FITT</li>
+  <li>Defenses</li>
+  <li>Damages</li>
+</ul>
+<p><strong>B. Negligence</strong></p>
+<p><strong>Approach</strong></p>
+<ul>
+  <li>General Duty
+    <ul>
+      <li>Cardoza View</li>
+      <li>Andrews View</li>
+    </ul>
+  </li>
+  <li>Special Duty
+    <ul>
+      <li>Violation of Statute</li>
+      <li>Guest Statute</li>
+      <li>Omission to act</li>
+      <li>L/O of Land</li>
+      <li>Duties Owed By Lessors of Land</li>
+      <li>V-Gold</li>
+    </ul>
+  </li>
+  <li>Breach</li>
+  <li>Causation
+    <ul>
+      <li>Actual Cause</li>
+      <li>Proximate Cause</li>
+    </ul>
+  </li>
+  <li>Damages
+    <ul>
+      <li>General Damages<br>
+        Past, present, future pain and suffering
+      </li>
+      <li>Special Damages<br>
+        Past, present, future economic losses, medical bills, loss of wages / profits
+      </li>
+      <li>Punitive Damages<br>
+      No recovery in negligence action</li>
+      <li>Avoidable Consequence Rule</li>
+      <li>Multiple Defendant Issues
+        <ul>
+          <li>Joint and several liability</li>
+          <li>Contributions</li>
+          <li>Indemnity</li>
+          <li>Satisfaction</li>
+          <li>Release</li>
+        </ul>
+      </li>
+      <li>NIED (Negligent Infliction of Emotional Distress </li>
+    </ul>
+  </li>
+  <li>Defenses 
+    <ul>
+      <li>Contributory Negligence</li>
+      <li>Comparative Negligence</li>
+      <li>Last Clear Chance</li>
+      <li>Assumption of Risk</li>
+    </ul>
+  </li>
+</ul>
+<p>C.<strong> Products Liability</strong></p>
+<p><strong>Approach</strong></p>
+<ol>
+  <li>General Products Liability (Can A prevail against B for products liability) ?<br>
+    <ol>
+      <li>Manufacturer or Reseller</li>
+      <li>An Unreasonably Dangerous Product</li>
+      <li>Released into the stream of commerce.</li>
+      <li>Which caused injury to person or property</li>
+      <li>Conclusion: Therefore A may be able to prevail against B in a product liability claim.<br>
+        <br>
+      </li>
+    </ol>
+  </li>
+  <li>Defect (Was the Widget Defective in Design, Manufacture or Warning) ?
+    <ol>
+      <li>Design Defect</li>
+      <li>Manufacture Defect</li>
+      <li>Warning defect<br>
+        <br>
+      </li>
+    </ol>
+  </li>
+  <li><strong>Theories (Can A prevail under the products theory of battery, negligence, breach of warranty, strict liability in torts)?</strong>
+<ol>
+      <li><strong>Battery</strong><br>
+        In order for the plaintiff to recover on a theory of intent (battery), the plaintiff must prove that the manufacturer, distributor or supplier knew the product would cause harm.        <br>
+      </li>
+      <li><strong>Negligence</strong><br>
+        In order for a plaintiff to recover under a negligence theory of products liability, the plaintiff will have to prove duty, breach, causation and damages.
+<ol>
+          <li><strong>Duty</strong><br>
+            Originally extended only to those in privity of contract, but under MacPherson v. Buick, the duty was extended to all foreseeable users.            <br>
+          </li>
+          <li><strong>Breach</strong><br>
+          P must show the product failed to meet ordinary commercial expectation          </li>
+          <li><strong>Causation</strong><br>
+            P must show that D was the actual and proximate cause.
+            <ol>
+              <li>Actual Cause</li>
+              <li>Proximate Cause<br>
+              </li>
+            </ol>
+          </li>
+          <li><strong>Damages</strong><br>
+          Limited to personal and property. Punitive available for reckless conduct.          </li>
+          <li><strong>MacPherson v. Buick</strong> <br>
+          In this landmark case, Justice Cardozo dispensed with the previously applied requirement of privity of contract in products liability cases and extended the manufacturer’s duty of care to include the ultimate consumer of the product, whether or not the consumer was the actual purchaser of the product. Later cases extended the duty to include not just the purchaser or the ultimate consumer, but to include all persons and property likely to be endangered by the product's probable use, such as the consumer's family or guests, or even near bystanders.</li>
+        </ol>
+      </li>
+      <li><strong>Breach of Warranty</strong><br>
+        Liability can be imposed against the manufacturer or seller of a product based upon the buyer’s <strong>reliance</strong> upon <strong>express and / or implied warranties</strong> that the goods are of <strong>merchantable quality</strong> and are fit for their <strong>intended purpose</strong>.
+        <ol>
+          <li>IRAC Breach of Warranty
+            <ol>
+              <li><strong>Implied promise</strong> by a merchant selling goods that product is fair and average quality, </li>
+              <li><strong>Fit </strong>for normal use</li>
+              <li>Emphasis is on <strong>safety</strong><br>
+                <strong>Example: </strong>The implied warranty relating to a car would likely include that its safety devices (airbag) would protect, not harm, the occupants. A impliedly warranted that the airbag was safe when it sold the car with this safety device.
+              Moreover, the addition of the inexpensive safety switch would have added to the reasonableness of the airbag's safety.</li>
+              <li><strong>Intended Purpose</strong><br>
+                Example: If the seller knows of buyer's intended use, and buyer acting as an average reasonable person, relies upon seller's &quot;special knowledge and skills,&quot; then a breach has occured.
+              </li>
+              <li><strong>Breach of Warranty</strong>, why it is not safe?<br>
+              <strong>Example: </strong>A breached the warranty by equipping the Roadster with an airbag that created a danger to young passengers. </li>
+              <li><strong>Privity of contract</strong><br>
+              <strong>Example: </strong>Most courts hold that the privity of contract is not required to recover in tort for breach of the implied warranty. A user of a product, like C, can sue, even though she did not purchase the car. </li>
+              <li><strong>Causation</strong><br>
+                Example: As discussed, breach was the cause in fact and proximate cause of C's head injury.</li>
+              <li>Damages</li>
+              <li>Defenses<br>
+                Example: No defenses.
+              </li>
+            </ol>
+          </li>
+        </ol>
+      </li>
+      <li>Strict Liability  </li>
+    </ol>
+  </li>
+</ol>
+<p><strong>D. Strict Liability</strong></p>
+<p><strong>Approach</strong></p>
+<ul>
+  <li>Activity, Rylands vs Fletcher</li>
+  <li>R1 / R2 (factors)</li>
+  <li>Absolute Duty to Make Safe</li>
+  <li>Foreseeable P / Harm</li>
+  <li>Causation</li>
+  <li>Damages</li>
+  <li>Defenses</li>
+</ul>
+<p><strong>E. Defamation</strong></p>
+<p><strong>Approach</strong></p>
+<ul>
+  <li>PF Case</li>
+  <li>Damages</li>
+  <li>Defenses
+    <ul>
+      <li>Truth</li>
+      <li>Consent</li>
+      <li>Absolute</li>
+      <li>Qualified</li>
+      <li>Con. Priv's</li>
+    </ul>
+  </li>
+</ul>
+<p>F. Vicarious Liability</p>
+<p><strong>G. Invasion of Privacy</strong></p>
+<p><strong>Approach</strong></p>
+<ul>
+  <li>Appropriation</li>
+  <li>Intrusion</li>
+  <li>Public Disclose</li>
+  <li>FL Public Eye</li>
+  <li>Defenses
+    <ul>
+      <li>Consent</li>
+      <li>Truth - FL</li>
+      <li>Newsworthy</li>
+    </ul>
+  </li>
+  <li>IIED? </li>
+</ul>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
