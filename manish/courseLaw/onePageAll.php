04e9d0a4b2e2cb20dd3e691f264e564ff90a6950
Manish Khanchandani <manishkk74@gmail.com>
2017-09-17 17:18:41 -0700

    dd

---
+++ manish/courseLaw/onePageAll.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+    font-family:Verdana, Arial, Helvetica, sans-serif;
+    font-size:11px;
+}
+</style>
+</head>
+
+<body>
+<p>Torts</p>
+<table border="1" cellspacing="0" cellpadding="5">
+  <tr>
+    <td valign="top"><strong>INTENTIONAL TORTS </strong></td>
+    <td valign="top"><strong>NEGLIGENCE</strong></td>
+  </tr>
+  <tr>
+    <td valign="top">[ALWAYS state this in your discussion of the first intentional tort.  For TORTS an act is intentional if it is done for the PURPOSE of producing a RESULT, or if the actors knows that result will almost certainly occur! But the result does not necessarily have to be injury. All that matters is that the result CAUSES injury.]</td>
+    <td valign="top">Under tort law NEGLIGENCE is a failure to  exercise that degree of care that a reasonably prudent person would use in the same situation.  To prevail the plaintiff must usually prove DUTY, BREACH, ACTUAL and PROXIMATE CAUSATION and DAMAGES.</td>
+  </tr>
+  <tr>
+    <td valign="top"><strong>Assault:</strong><br>
+    Under tort law ASSAULT is an intentional act done to cause and that does cause  reasonable apprehension of a battery, a harmful or offensive touching of the person.</td>
+    <td valign="top">Duty</td>
+  </tr>
+  <tr>
+    <td valign="top"><strong>Battery:</strong><br>
+    Under tort law BATTERY is an intentional act to cause and does cause a touching of the person of the plaintiff resulting in harm or offense to the plaintiff.   Important!<br></td>
+    <td valign="top">Breach</td>
+  </tr>
+  <tr>
+    <td valign="top"><strong>Conversion:</strong><br>
+    Under tort law CONVERSION is  an intentional act to cause and that does cause interference with the chattel of the plaintiff resulting in substantial deprivation of possession.<br>
+    Note: The proscribed legal remedy is forced purchase by the defendant, but the plaintiff may “waive the tort” and seek restitution instead. </td>
+    <td valign="top">Causation</td>
+  </tr>
+  <tr>
+    <td valign="top">False Imprisonment: </td>
+    <td valign="top">Damages</td>
+  </tr>
+  <tr>
+    <td valign="top">Intentional Infliction of Emotional Distress? </td>
+    <td valign="top">Defenses</td>
+  </tr>
+  <tr>
+    <td valign="top">Trespass to Chattel </td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td valign="top">Tresspass to Land </td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td valign="top">&nbsp;</td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td valign="top"><strong>STRICT LIABILITY </strong></td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td valign="top">&nbsp;</td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+</table>
+<p>&nbsp; </p>
+</body>
+</html>
