d58f40f68d2524f112426b0a2562755502adf50a
Manish Khanchandani <manishkk74@gmail.com>
2017-11-13 17:03:59 -0800

    test

---
+++ manish/interview/facebook/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Syllabus</title>
+</head>
+
+<body>
+<a href="two_sums.php">1 Two Sum 23.0% Easy</a><br />
+10 Regular Expression Matching 22.0% Hard<br />
+13 Roman to Integer 39.2% Easy<br />
+15 3Sum 18.8% Medium<br />
+17 Letter Combinations of a Phone Number 28.6% Medium<br />
+20 Valid Parentheses 29.3% Easy<br />
+23 Merge k Sorted Lists 23.3% Hard<br />
+25 Reverse Nodes in k-Group 27.5% Hard<br />
+26 Remove Duplicates from Sorted Array 33.4% Easy<br />
+28 Implement strStr() 24.8% Easy<br />
+33 Search in Rotated Sorted Array 30.2% Hard<br />
+38 Count and Say 28.9% Easy<br />
+43 Multiply Strings 23.4% Medium<br />
+44 Wildcard Matching 17.4% Hard<br />
+49 Group Anagrams 27.4% Medium<br />
+50 Pow(x, n) 27.9% Medium<br />
+56 Merge Intervals 25.3% Hard<br />
+57 Insert Interval 23.8% Hard<br />
+67 Add Binary 27.4% Easy<br />
+69 Sqrt(x) 25.2% Medium<br />
+71 Simplify Path 21.9% Medium<br />
+75 Sort Colors 34.7% Medium<br />
+76 Minimum Window Substring 21.2% Hard<br />
+78 Subsets 31.3% Medium<br />
+79 Word Search 22.9% Medium<br />
+80 Remove Duplicates from Sorted Array II 32.7% Medium<br />
+85 Maximal Rectangle 23.4% Hard<br />
+88 Merge Sorted Array 30.0% Easy<br />
+90 Subsets II 30.5% Medium<br />
+91 Decode Ways 17.5% Medium<br />
+98 Validate Binary Search Tree 20.9% Medium<br />
+102 Binary Tree Level Order Traversal 32.7% Easy<br />
+117 Populating Next Right Pointers in Each Node II 32.8% Hard<br />
+121 Best Time to Buy and Sell Stock 36.0% Easy<br />
+125 Valid Palindrome 23.8% Easy<br />
+127 Word Ladder 19.6% Medium<br />
+128 Longest Consecutive Sequence 32.1% Hard<br />
+133 Clone Graph 24.9% Medium<br />
+139 Word Break 25.2% Medium<br />
+146 LRU Cache 15.8% Hard<br />
+157 Read N Characters Given Read4 29.5% Easy<br />
+158 Read N Characters Given Read4 II &ndash; Call multiple times 23.4% Hard<br />
+161 One Edit Distance 28.4% Medium<br />
+168 Excel Sheet Column Title 21.6% Easy<br />
+173 Binary Search Tree Iterator 34.5% Medium<br />
+200 Number of Islands 27.6% Medium<br />
+206 Reverse Linked List 39.4% Easy<br />
+208 Implement Trie (Prefix Tree) 25.3% Medium<br />
+209 Minimum Size Subarray Sum 26.8% Medium<br />
+210 Course Schedule II 21.0% Medium<br />
+211 Add and Search Word &ndash; Data structure design 20.2% Medium<br />
+215 Kth Largest Element in an Array 33.1% Medium<br />
+218 The Skyline Problem 22.0% Hard<br />
+221 Maximal Square 23.5% Medium<br />
+234 Palindrome Linked List 28.0% Easy<br />
+235 Lowest Common Ancestor of a Binary Search Tree 37.7% Easy<br />
+236 Lowest Common Ancestor of a Binary Tree 28.8% Medium<br />
+238 Product of Array Except Self 42.6% Medium<br />
+252 Meeting Rooms 41.6% Easy<br />
+253 Meeting Rooms II 34.6% Medium<br />
+257 Binary Tree Paths 28.4% Easy<br />
+261 Graph Valid Tree 32.5% Medium<br />
+265 Paint House II 35.6% Hard<br />
+269 Alien Dictionary 22.9% Hard<br />
+273 Integer to English Words 18.7% Hard<br />
+274 H-Index 29.3% Medium<br />
+275 H-Index II 32.5% Medium<br />
+277 Find the Celebrity 35.1% Medium<br />
+278 First Bad Version 22.4% Easy<br />
+282 Expression Add Operators 24.8% Hard<br />
+283 Move Zeroes 44.0% Easy<br />
+285 Inorder Successor in BST 35.6% Medium<br />
+286 Walls and Gates 37.2% Medium<br />
+297 Serialize and Deserialize Binary Tree 27.8% Hard<br />
+301 Remove Invalid Parentheses 31.9% Hard<br />
+311 Sparse Matrix Multiplication 47.2% Medium<br />
+314 Binary Tree Vertical Order Traversal 30.3% Medium<br />
+325 Maximum Size Subarray Sum Equals k 39.9% Easy<br />
+334 Increasing Triplet Subsequence 33.2% Medium
+</body>
+</html>
---
+++ manish/interview/facebook/two_sums.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Two Sums</title>
+<script language="javascript">
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = (nums, target) => {
+    console.log('nums are ', nums);
+    console.log('target are ', target);
+	let result = [];
+	
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[j] === target - nums[i]) {
+				result.push([nums[i], nums[j]]);
+				result.push([i, j]);
+			}
+		}
+	}
+	
+	return result;
+};//complexity of O(n²)
+
+console.log(twoSum([2, 11, 15, 7], 9));
+
+/*
+though process
+Here is my thought process upon first look…
+Step 1 Take first number in array, which in this case is -4
+Step 2 Look through the rest of the array for any number that, when added to -4, gives us the target, 3
+Step 3 If I find a number that satisfies the above condition, I will jot down the two numbers as a pair
+Step 4 Repeat steps 1 to 3 for the rest of the numbers in the array
+
+
+*/
+</script>
+</head>
+
+<body>
+<p>Two Sums</p>
+<p>Given an array of integers, return&nbsp;<strong>indices</strong>&nbsp;of the two numbers such that they add up to a specific target.</p>
+<p>You may assume that each input would have&nbsp;<strong><em>exactly</em></strong>&nbsp;one solution, and you may not use the&nbsp;<em>same</em>&nbsp;element twice.</p>
+<p><strong>Example:</strong><br />
+</p>
+<pre>Given nums = [2, 7, 11, 15], target = 9,    
+Because nums[<strong>0</strong>] + nums[<strong>1</strong>] = 2 + 7 = 9,  
+return [<strong>0</strong>, <strong>1</strong>].</pre>
+<p>Solution:</p>
+<p>&nbsp;</p>
+</body>
+</html>
